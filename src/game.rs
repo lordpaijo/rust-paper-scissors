b44120e2bf4de466c743cd1d80c6fc0a299c94d9
lordpaijo <iyanmaumakan56@gmail.com>
2025-02-10 12:55:08 +0700

    using modules

---
+++ src/game.rs
@@ -0,0 +1,53 @@
+use rand::Rng;
+use std::io;
+use colored::*;
+
+pub fn Game() {
+    println!("{}", "\nwelcome to rock, paper, scissors!\n".yellow());
+    let choices = ["rock", "paper", "scissors"];
+    let mut user: i32 = 0;
+    let mut com: i32 = 0;
+    let mut ties: i32 = 0;
+    let mut rounds: u64 = 1;
+    loop {
+        Header(user, com, ties, rounds);
+        let mut user_input = String::new();
+        io::stdin().read_line(&mut user_input).expect("Failed to read input");
+        let user_choice = user_input.trim().to_lowercase();
+        
+        if !choices.contains(&user_choice.as_str()) && 
+          user_choice.as_str().trim().to_lowercase() != "exit" {
+            println!("{}","Invalid choice! Please enter rock, paper, or scissors.".red()); 
+            return;
+        }   if user_choice.as_str().trim().to_lowercase() == "exit"
+            { println!("{} {}", "\nExitting game,".red(), "goodbye!".green()); break; }
+        Logic(&mut user, &mut com, &mut ties, &mut rounds, &choices, user_choice);
+    }
+}
+
+
+fn Header(user: i32, com: i32, ties: i32, rounds: u64) {
+    println!("{}{}", "round: ".yellow(), rounds);
+    println!("{}\t {}: {}\t {}: {}\t {}: {}", 
+        "enter your choice (rock, paper, scissors):".cyan(), 
+        "you".green(), user, "com".red(), com, "ties".yellow(), ties);
+}
+
+fn Logic(user:&mut i32, com:&mut i32, ties:&mut i32, rounds:&mut u64, 
+  choices: &[&str], user_choice: String) {
+    let computer_choice = choices[rand::rng().random_range(0..3)];
+    match (user_choice.as_str(), computer_choice) {
+        ("rock", "scissors") | ("paper", "rock") | ("scissors", "paper") => { 
+            println!("{}", "\nYou win!".green()); 
+            println!("{} chose: {}\n", "Computer".red(), computer_choice);
+            *user += 1; }
+        ("rock", "rock") | ("paper", "paper") | ("scissors", "scissors") => { 
+            println!("{}", "\nIt's a tie!".yellow()); 
+            println!("{} chose: {}\n", "Computer".red(), computer_choice);
+            *ties += 1; }
+        _ => { 
+            println!("{}", "\nComputer wins!".red());                         
+            println!("{} chose: {}\n", "Computer".red(), computer_choice);
+            *com += 1 }
+    }  *rounds += 1;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,9 @@
-use rand::Rng;
-use std::io;
-use colored::*;
+mod game;
 
-fn main() {
-    println!("{}", "\nWelcome to Rock, Paper, Scissors!\n".yellow());
-    let choices = ["rock", "paper", "scissors"];
-    let mut user: i32 = 0;
-    let mut com: i32 = 0;
-    let mut ties: i32 = 0;
-    let mut round: u64 = 1;
+fn main() { 
     loop {
-        println!("{}{}", "Round: ".yellow(), round);
-        println!("{}\t {}: {}\t {}: {}\t {}: {}", 
-            "Enter your choice (rock, paper, scissors):".cyan(), 
-            "You".green(), user, "Com".red(), com, "Ties".yellow(), ties);
-
-        let mut user_input = String::new();
-        io::stdin().read_line(&mut user_input).expect("Failed to read input");
-        let user_choice = user_input.trim().to_lowercase();
-        
-        if !choices.contains(&user_choice.as_str()) && 
-          user_choice.as_str().trim().to_lowercase() != "exit" {
-            println!("{}","Invalid choice! Please enter rock, paper, or scissors.".red()); 
-            return;
-        }   if user_choice.as_str().trim().to_lowercase() == "exit"
-            { println!("{} {}", "\nExitting game,".red(), "goodbye!".green()); break; }
-
-        let computer_choice = choices[rand::rng().random_range(0..3)];
-        match (user_choice.as_str(), computer_choice) {
-            ("rock", "scissors") | ("paper", "rock") | ("scissors", "paper") => { 
-                println!("{}", "\nYou win!".green()); 
-                println!("{} chose: {}\n", "Computer".red(), computer_choice);
-                user += 1; }
-            ("rock", "rock") | ("paper", "paper") | ("scissors", "scissors") => { 
-                println!("{}", "\nIt's a tie!".yellow()); 
-                println!("{} chose: {}\n", "Computer".red(), computer_choice);
-                ties += 1; }
-            _ => { 
-                println!("{}", "\nComputer wins!".red());                         
-                println!("{} chose: {}\n", "Computer".red(), computer_choice);
-                com += 1 }
-        }   round += 1;
+        game::Game();
+        break;
     }
 }
 
